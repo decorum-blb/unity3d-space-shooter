0ac5b2eb28609845374ca0e1acc8ee1b36e47744
Adam Edwards <adamcharlesedwards@gmail.com>
2018-09-16 21:58:00 -0500

    initial commit of project. On tutorial stage: 03.01 (Audio)

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Boundary") {
+            return;
+        }
+
+        Transform transform = GetComponent<Transform>();
+
+        if (other.tag == "Player") {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        Destroy(other.gameObject); // Destroy Bolts as they collide with the Asteroid
+        Destroy(this.gameObject); // Destroy the Asteroid itself
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float startWait;
+    public float spawnWait;
+	public float waveWait;
+
+    void Start() {
+        StartCoroutine(SpawnWaves());
+    }
+    IEnumerator SpawnWaves() {
+        yield return new WaitForSeconds(startWait);
+
+        while (true) {
+            for (int i = 0; i < hazardCount; i++) {
+                createHazard();
+                yield return new WaitForSeconds(spawnWait);
+            }
+        }
+
+		yield return new WaitForSeconds(waveWait);
+    }
+
+    private GameObject createHazard() {
+        float value = Random.Range(-spawnValues.x, spawnValues.x);
+
+        Vector3 spawnPosition = new Vector3(value, spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+
+        return Instantiate(hazard, spawnPosition, spawnRotation) as GameObject;
+    }
+}
